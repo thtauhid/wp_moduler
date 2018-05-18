eadfb6d1e7b5a80ca1613c4f0ffc3742bad957bc
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-05-18 23:02:53 +0600

    Code update :)

--- author.php
+++ author.php
@@ -5,6 +5,10 @@
 
   <div class="row">
     <!--Blog Update-->
+    <div class="col-12">
+    	<?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
+    	<h2><p>Author: <?php echo $curauth->nickname; ?></p></h2>
+    </div>
     <?php if(have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
 
@@ -16,8 +20,7 @@
       if(has_post_thumbnail()) {the_post_thumbnail_url();} else{echo "http://via.placeholder.com/380x210";} ?>"></a>
        <div class="card-body">
         <h4 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
-        <p>On <?php the_time('j F, Y') ?> by <?php the_author_posts_link(); ?> </p>
-
+        <p>On <?php the_time('j F, Y') ?> by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></p>
         <p class="card-text"><?php the_excerpt() ?></p>
           <button type="button" class="a btn btn-outline-primary"><a href="<?php the_permalink() ?>">More >></a></button><br>
       </div>
--- footer.php
+++ footer.php
@@ -2,31 +2,9 @@
   <div class="row">
     <!--Footer-->
     <div class="col-12">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-faded">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" style="">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarColor02">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-          </ul>
-        </div>
-                <p>
-                        &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ) ?> | Powered by <a href="https://wordpress.org">Wordpress</a>
-                </p>
-      </nav>
+        <p class="float-right">
+           &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ) ?> | Powered by <a href="https://wordpress.org">Wordpress</a>
+        </p>
       <br>
   </div>
 </div>
