7a07e1281344e615fd51c14e0533aab0bd58a20a
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-05-19 01:04:27 +0600

    Code Update :)

--- functions.php
+++ functions.php
@@ -5,6 +5,21 @@ register_nav_menus( array(
 	'primary' => __( 'Primary Menu', 'moduler' ),
 ) );
 
+	// //register widget for front page
+	// register_sidebar( array( 
+	// 	'name' => '', 
+	// 	'id' => '', 
+	// 	'before_widget' => '', 
+	// 	'after_widget' => '', 
+	// 	'before_title' => '', 
+	// 	'after_title' => '' 
+	// ) );
+
+
+
+
+
+
 
 //Excerpt Length
 function excerpt_length()	{
@@ -13,3 +28,5 @@ function excerpt_length()	{
 
 add_filter('excerpt_length', 'excerpt_length');
 add_theme_support('post-thumbnails');
+
+require get_template_directory(). '/inc/customizer.php';
