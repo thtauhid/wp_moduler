b4da014066fe1ffa572bcf223ee0cb1c48666cc4
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-05-21 04:32:02 +0600

    Code Update :)

--- inc/customizer.php
+++ inc/customizer.php
@@ -415,3 +415,446 @@ function moduler_customize_register_client_area($wp_customize) {
 }
 
 add_action('customize_register', 'moduler_customize_register_client_area');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function moduler_customize_register_our_team($wp_customize) {
+
+
+	//registering section -> Our Team
+	$wp_customize->add_section('Team', array(
+		'title'	=>	__('Our Team', 'moduler'),
+		'description'	=>	__('Our Team Options', 'moduler'),
+		'priority'	=>	'160',
+	));
+
+
+
+	//Title
+	$wp_customize->add_setting('our_team_main_title', array(
+		'default' => __('Our Awesome Team', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('our_team_main_title', array(
+		'label' => __('Title', 'moduler'),
+		'section' => 'Team',
+		'priority' => '1'
+	));
+
+
+	//Team member 01
+
+		//Image
+		$wp_customize->add_setting('team_member_1_img', array(
+			'default' => get_bloginfo('template_directory'). '/assets/placeholder_face.png',
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_1_img', array(
+			'label' => __('Team Memmber 01 Image:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '2'
+		)));
+
+
+
+		//Name
+		$wp_customize->add_setting('team_member_1_name', array(
+			'default' => __('Tasnimul H. Tauhid', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_name', array(
+			'label' => __('Team Member 01 Name:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '3'
+		));
+
+
+
+		//Designation
+		$wp_customize->add_setting('team_member_1_designation', array(
+			'default' => __('Web Developer', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_designation', array(
+			'label' => __('Team Member 01 Designation:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '4'
+		));
+
+
+
+		//Text
+		$wp_customize->add_setting('team_member_1_text', array(
+			'default' => __('Some quick example text to build on the card title and make up the bulk of the cards content.', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_text', array(
+			'label' => __('Team Member 01 Text:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '5'
+		));
+
+
+		//FB
+		$wp_customize->add_setting('team_member_1_facebook', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_facebook', array(
+			'label' => __('Team Member 01 FB ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '6'
+		));
+
+
+		//Twitter
+		$wp_customize->add_setting('team_member_1_twitter', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_twitter', array(
+			'label' => __('Team Member 01 Twitter ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '7'
+		));
+
+
+
+
+		//Email
+		$wp_customize->add_setting('team_member_1_email', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_1_email', array(
+			'label' => __('Team Member 01 Email ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '8'
+		));
+
+
+
+		//Team member 02
+
+		//Image
+		$wp_customize->add_setting('team_member_2_img', array(
+			'default' => get_bloginfo('template_directory'). '/assets/placeholder_face.png',
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_2_img', array(
+			'label' => __('Team Memmber 02 Image:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '9'
+		)));
+
+
+
+		//Name
+		$wp_customize->add_setting('team_member_2_name', array(
+			'default' => __('Tasnimul H. Tauhid', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_name', array(
+			'label' => __('Team Member 02 Name:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '10'
+		));
+
+
+
+		//Designation
+		$wp_customize->add_setting('team_member_2_designation', array(
+			'default' => __('Web Developer', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_designation', array(
+			'label' => __('Team Member 02 Designation:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '11'
+		));
+
+
+
+		//Text
+		$wp_customize->add_setting('team_member_2_text', array(
+			'default' => __('Some quick example text to build on the card title and make up the bulk of the cards content.', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_text', array(
+			'label' => __('Team Member 02 Text:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '12'
+		));
+
+
+		//FB
+		$wp_customize->add_setting('team_member_2_facebook', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_facebook', array(
+			'label' => __('Team Member 02 FB ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '13'
+		));
+
+
+		//Twitter
+		$wp_customize->add_setting('team_member_2_twitter', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_twitter', array(
+			'label' => __('Team Member 02 Twitter ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '14'
+		));
+
+
+
+
+		//Email
+		$wp_customize->add_setting('team_member_2_email', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_2_email', array(
+			'label' => __('Team Member 02 Email ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '15'
+		));
+
+
+		//Team member 03
+
+		//Image
+		$wp_customize->add_setting('team_member_3_img', array(
+			'default' => get_bloginfo('template_directory'). '/assets/placeholder_face.png',
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'team_member_3_img', array(
+			'label' => __('Team Memmber 03 Image:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '16'
+		)));
+
+
+
+		//Name
+		$wp_customize->add_setting('team_member_3_name', array(
+			'default' => __('Tasnimul H. Tauhid', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_name', array(
+			'label' => __('Team Member 03 Name:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '17'
+		));
+
+
+
+		//Designation
+		$wp_customize->add_setting('team_member_3_designation', array(
+			'default' => __('Web Developer', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_designation', array(
+			'label' => __('Team Member 03 Designation:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '18'
+		));
+
+
+
+		//Text
+		$wp_customize->add_setting('team_member_3_text', array(
+			'default' => __('Some quick example text to build on the card title and make up the bulk of the cards content.', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_text', array(
+			'label' => __('Team Member 03 Text:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '19'
+		));
+
+
+		//FB
+		$wp_customize->add_setting('team_member_3_facebook', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_facebook', array(
+			'label' => __('Team Member 03 FB ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '20'
+		));
+
+
+		//Twitter
+		$wp_customize->add_setting('team_member_3_twitter', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_twitter', array(
+			'label' => __('Team Member 03 Twitter ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '21'
+		));
+
+
+
+
+		//Email
+		$wp_customize->add_setting('team_member_3_email', array(
+			'default' => __('', 'moduler'),
+			'type' => 'theme_mod',
+		));
+
+		$wp_customize->add_control('team_member_3_email', array(
+			'label' => __('Team Member 03 Email ID:', 'moduler'),
+			'section' => 'Team',
+			'priority' => '22'
+		));
+}
+
+add_action('customize_register', 'moduler_customize_register_our_team');
+
+
+
+
+
+
+function moduler_customize_register_cta($wp_customize) {
+
+
+	//registering section -> CTA
+	$wp_customize->add_section('CTA', array(
+		'title'	=>	__('CTA', 'moduler'),
+		'description'	=>	__('CTA Options', 'moduler'),
+		'priority'	=>	'170',
+	));
+
+
+
+	//Title
+	$wp_customize->add_setting('cta_title', array(
+		'default' => __('The Call To Action Button', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('cta_title', array(
+		'label' => __('Title', 'moduler'),
+		'section' => 'CTA',
+		'priority' => '1'
+	));
+
+
+
+	//Sub Title
+	$wp_customize->add_setting('cta_sub_title', array(
+		'default' => __('This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('cta_sub_title', array(
+		'label' => __('CTA Sub Title', 'moduler'),
+		'section' => 'CTA',
+		'priority' => '2'
+	));
+
+
+
+
+
+	//Main Text
+	$wp_customize->add_setting('cta_main_text', array(
+		'default' => __('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('cta_main_text', array(
+		'label' => __('Main Text', 'moduler'),
+		'section' => 'CTA',
+		'priority' => '3'
+	));
+
+
+
+
+
+
+	//Button Text
+	$wp_customize->add_setting('cta_button_text', array(
+		'default' => __('Check out', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('cta_button_text', array(
+		'label' => __('Button Text', 'moduler'),
+		'section' => 'CTA',
+		'priority' => '4'
+	));
+
+
+
+
+
+
+	//Button URL
+	$wp_customize->add_setting('cta_button_url', array(
+		'default' => __('#', 'moduler'),
+		'type' => 'theme_mod',
+	));
+
+	$wp_customize->add_control('cta_button_url', array(
+		'label' => __('Button URL', 'moduler'),
+		'section' => 'CTA',
+		'priority' => '5'
+	));
+
+}
+
+add_action('customize_register', 'moduler_customize_register_cta');
