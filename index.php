53a6209af78c4dc069e3946f378b3b250c803b48
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-05-30 01:06:23 +0600

    Placeholder image updated.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="card mb-4">
      <a href="<?php the_permalink(); ?>"> <img style="height: 210px; width: 100%" class="img-thumbnail" src="
       <?php 
-      if(has_post_thumbnail()) {the_post_thumbnail_url();} else{echo "http://via.placeholder.com/380x210";} ?>"></a>
+      if(has_post_thumbnail()) {the_post_thumbnail_url();} else{ echo get_template_directory_uri() . '/assets/binder-blank-book-bindings-479815.jpg'; } ?>"></a>
        <div class="card-body">
         <h4 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
         <p><?php echo __('On ', 'moduler'); the_time('j F, Y');  echo __(' by ', 'moduler'); the_author_posts_link(); ?> </p>
