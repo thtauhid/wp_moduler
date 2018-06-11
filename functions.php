8d57ebcddebc8aa8f9de9faed28578ea762c4098
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-06-12 02:31:05 +0600

    Structural update

--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php
-if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
-	// file does not exist... return an error.
-	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'moduler' ) );
-} else {
-	// file exists... require it.
-	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
-}
+require_once get_template_directory() . 'assets/classes/class-wp-bootstrap-navwalker.php';
 
 register_nav_menus( array(
 	'primary' => __( 'Primary Menu', 'moduler' ),
