e6eeadf245a5f693898c13d2c8d235c167af9199
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2018-06-14 23:00:45 +0600

    Bug fix

--- header.php
+++ header.php
@@ -16,23 +16,22 @@
     <!--Header-->
     <div class="col-12">
       <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="<?php echo esc_url( home_url() ) ?>"><?php bloginfo('name'); ?></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <a class="navbar-brand" href="<?php echo esc_url( home_url() ) ?>"><?php bloginfo('name'); ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarColor01">
-   <?php wp_nav_menu( array(
+  <?php wp_nav_menu( array(
     'theme_location'  => 'primary',
     'depth'              => 2, // 1 = no dropdowns, 2 = with dropdowns.
     'container'       => 'div',
     'container_class' => 'collapse navbar-collapse',
-  'container_id'    => 'bs-example-navbar-collapse-1',
-  'menu_class'      => 'navbar-nav mr-auto',
-  'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-  'walker'          => new WP_Bootstrap_Navwalker(),
+    'container_id'    => 'navbarColor01',
+    'menu_class'      => 'navbar-nav mr-auto',
+    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+    'walker'          => new WP_Bootstrap_Navwalker(),
 ) );
 ?>
-    <?php get_search_form(); ?>
-  </div>
+<?php get_search_form(); ?>
+
 </nav>
